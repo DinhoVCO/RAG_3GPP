ca97295d07bf4d3a15c9118d88d0c9a835c3351e
Ronaldinho Vega Centeno Olivera <dinho15971@gmail.com>
2025-01-18 22:18:35 -0500

    task-7:fix path

--- run/phi_ft_1.sh
+++ run/phi_ft_1.sh
@@ -2,6 +2,6 @@ python /content/RAG_3GPP/RAG/main_rag.py \
   --inference_type "llm" \
   --reader_model_name "dinho1597/phi-2-telecom-v1.1" \
   --test_dataset_name "dinho1597/3GPP-QA-MultipleChoice" \
-  --output_csv_path "/content/drive/MyDrive/Papers/RAG_3GPP/results/phi_ft_v1_results.csv" \
+  --output_csv_path "/content/drive/MyDrive/Papers/RAG_3GPP/results/phi_ft_1_results.csv" \
   --batch_size 100 \
   --llm_batch_size 20
